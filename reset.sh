d08f12e0dd7ba5d6000432c9b393642cf89b7279
Steven Nuñez <steven@flatironschool.com>
2018-09-18 11:17:41 -0400

    Update for Mac

--- reset.sh
+++ reset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker-compose restart db-perfecto
 mix do ecto.drop, ecto.create
-cat priv/seed.sql | docker-compose exec -u postgres db-perfecto psql ecto_perfecto
+cat priv/seed.sql | pbcopy
+echo "******************Paste what's in your buffer*************"
+docker-compose exec -u postgres db-perfecto psql ecto_perfecto
--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 docker volume create --name=db-perfecto-data
 docker-compose up -d
 mix do deps.get, ecto.create
-cat priv/seed.sql | docker-compose exec -u postgres db-perfecto psql ecto_perfecto
+cat priv/seed.sql | pbcopy
+echo "******************Paste what's in your buffer*************"
+docker-compose exec -u postgres db-perfecto psql ecto_perfecto
