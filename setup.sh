bdcf89cc371079acaa3907b4dc082b0a86a50a5b
Steven Nunez <steven.nunez@gmail.com>
2018-09-18 09:18:55 -0400

    Start Containers before running ecto commands

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mix do deps.get, ecto.create
 docker volume create --name=db-perfecto-data
 docker-compose up -d
+mix do deps.get, ecto.create
 cat priv/seed.sql | docker-compose exec -u postgres db-perfecto psql ecto_perfecto
